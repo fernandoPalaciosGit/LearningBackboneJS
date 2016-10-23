6c83fc5b2333442f84f7c8c348fd052f933af187
Juan David Hermoso <juandavidhermoso@gmail.com>
2016-10-23 12:02:30 +0200

    I'm getting undefined when trying to access model from view

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,12 +1,8 @@
 (function (w, d) {
   var Todo = Backbone.Model.extend({
       defaults: {
-        items: [
-          {
-            title: '',
-            completed: false
-          }
-        ]
+        title: '',
+        completed: false
       },
       initialize: function () {
         this.on('invalid', function (model, error) {
@@ -22,6 +18,15 @@
     todo1 = new Todo();
 
   var TodoView = Backbone.View.extend({
-    
+    el: '#todoItems',
+    initialize: function() {
+      this.model.bind('change', _.bind(this.render, this));
+    }
   });
+
+  todo1.set({
+    title: 'title'
+  });
+
+  var todo1View = new TodoView();
 })(window, document);
