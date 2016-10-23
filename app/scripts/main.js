465670ea9e211617ca4aa0a43454a6fbd7ba142f
Juan David Hermoso <juandavidhermoso@gmail.com>
2016-10-23 11:29:40 +0200

    View cleaned

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,8 +1,12 @@
 (function (w, d) {
   var Todo = Backbone.Model.extend({
       defaults: {
-        title: 'Title value',
-        completed: false
+        items: [
+          {
+            title: '',
+            completed: false
+          }
+        ]
       },
       initialize: function () {
         this.on('invalid', function (model, error) {
@@ -18,40 +22,6 @@
     todo1 = new Todo();
 
   var TodoView = Backbone.View.extend({
-    todoTpl: _.template('My todo template'),
-
-    //Listening events over the template parts!
-
-    events: {
-      'dblclick label': 'edit',
-      'keypress .edit': 'updateOnEnter',
-      'blur .edit': 'close'
-    },
-    initialize: function (options) {
-      this.options = options || {};
-    },
-    render: function () {
-      this.$el.html(this.todoTpl(this.model.attributes));
-      this.input = this.$('.edit');
-      return this;
-    },
-    edit: function () {
-      //Db click event
-    },
-
-    close: function () {
-      //Blur event
-    },
-
-    updateOnEnter: function (e) {
-      //Keypress event
-    }
+    
   });
-
-  var todoView = new TodoView({
-    el: $('#todosContainer')
-  });
-
-  // todoView.el is a reference to the DOM element that corresponds to the instance of the view. In this case, <li>
-  console.log(todoView.el);
 })(window, document);
