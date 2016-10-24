c71f3a355a1ccf8f2798bb20a9d5e67aa45a3ed6
unknown <jhermoso@VDI-EDREX64_5.brujula.es>
2016-10-24 17:03:02 +0200

    Collections test

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,23 +1,43 @@
 (function (w, d) {
-  //Collections:
-  var Todo = Backbone.Model.extend({
+  //Create a model type for instantiate from. (I've changed the concept to book, instead of to do tasks, only for this example).
+  var Book = Backbone.Model.extend({
     defaults: {
-      title: '',
-      completed: false
+      title: 'Developing Backbone JS apps',
+      author: 'Addy Osmani'
     }
   });
-  var TodosCollection = Backbone.Collection.extend({
-    //This is how We specify which type of models is gonna contain this collection.
-    model: Todo
+
+  //Create a collection type, passing in wich type of models is gonna contain this collection.
+  var Library = Backbone.Collection.extend({
+    model: Book
   });
 
-  var task1 = new Todo({
-    title: 'Learn Backbonejs', id: 1
-  });
-  console.log(task1.attributes);
+  //Create three books models.
+  var a = new Book({
+ title: 'Javascript Patterns'
+ }),
+    b = new Book({
+ title: 'Javascript- The good parts'
+ }),
+    c = new Book({
+ title: 'Eloquent javascript'
+ });
 
-  var todosList = new TodosCollection([task1]);
+  //I instantantiate my collection / library with 2 models / books
+  var libraryCatalog = new Library([a,b]);
 
-  //For now, obviously, there's 1 task (model) added.
-  console.log('Tasks added: ' + todosList.length);
+  //Notice since console is async, here I can't find the models in the model array cause I've removed them above!!
+  console.log(libraryCatalog);
+
+  //I add the third model to the collection
+  libraryCatalog.add(c);
+
+  //I should have 3 items / models in this collection
+  console.log('Collection size: ' + libraryCatalog.length);
+  //I remove 2 items from the collection, therefore... Size should be 1.
+  libraryCatalog.remove([a,b]);
+  console.log('Collection size: ' + libraryCatalog.length);
+  //I remove 1 item from the collection.. So... It should have 0 items, now.
+  libraryCatalog.remove(c);
+  console.log('Collection size: ' + libraryCatalog.length);
 })(window, document);
