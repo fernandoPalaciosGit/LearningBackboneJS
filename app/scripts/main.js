4c701a87024e032e72c9041b06a73346d64e451a
jdavidhermoso <juandavidhermoso@gmail.com>
2016-10-25 12:50:49 +0200

    With Collection.fetch() We can get models from the server

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,78 +1,18 @@
 (function (w, d) {
 
-  var TasksList = Backbone.Collection.extend({
-    filterById: function(ids){
-      return this.filter(
-        function(c) {
-          return _.contains(ids, c.id);
-        })
+  //Collection.fetch gets a set of models from the server in a JSON format array to the URL set in the collection URL property.
+
+  var Task = Backbone.Model.extend({
+    defaults: {
+      title: '',
+      completed: false
     }
   });
 
-
-  //I'm gona use this collection.
+  var TasksList = Backbone.Collection.extend({
+    model: Task,
+    url: '/tasks'
+  });
   var tasks = new TasksList();
-  tasks.add([
-    {
-      id: 1,
-      title: 'Try Backbone',
-      completed: true
-    },
-    {
-      id: 2,
-      title: 'Go to buy some clothes',
-      completed: false
-    },
-    {
-      id: 3,
-      title: 'Learn English',
-      completed: false
-    }
-  ]);
-
-  //Chainable API.
-
-  //chain() mehtod allows to chain calls to the Underscore methods on the collections.
-  //chain() method returns an object with all Underscore functions as a method.
-  //chain ends calling to value(), and it returns the final array of values with the Underscore functions 'applied'.
-
-  //This statement returns a wrapper around collections model
-  var filteredTasks = tasks.chain();
-  console.log(filteredTasks);
-
-  //Like this, I'm filtering to get only the task(s) whose completed is true, but I'm still getting the wrapper object (nor an array of models), cause I'm not appliying the value() function.
-  console.log(filteredTasks.filter(function(task) {
-    return task.get('completed') == true;
-  }));
-
-  console.log('Same code than below. Unique difference is that I close the chain using value(), so I get an array of models whose completed property is true.');
-  console.log(filteredTasks.filter(function(task) {
-    return task.get('completed') == true;
-  }).value());
-
-  //This should return an array of titles of the tasks whose completed property is true!!
-  console.log(filteredTasks
-    .filter(function(task) {
-      return task.get('completed') == true;
-    })
-    .map(function(task) {
-      return task.get('title');
-    }).value());
-
-
-  //We can add som of the Backbone-specific methods. Such as add, set, and remove methods.
-  //It's not working for me, right now :/
-
-  tasks
-    .add({
-      title: 'New task 1',
-      completed: false
-    })
-    .add({
-      title: 'New Task 2'
-    });
-
-  console.log(tasks.models);
-  debugger;
-
+  tasks.fetch();
 })(window, document);
