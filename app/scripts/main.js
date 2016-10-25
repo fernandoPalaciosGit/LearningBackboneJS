1ea4a6c54a0b48dac658b195afde0a5a309c204f
jdavidhermoso <juandavidhermoso@gmail.com>
2016-10-25 11:25:58 +0200

    Underscore utilities: Foreach over collections

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,2 +1,24 @@
 (function (w, d) {
+  //foreach is for iterate over collections.
+  var tasks = new Backbone.Collection();
+  tasks.add([
+    {
+      title: 'Go to buy some clothes',
+      completed: false
+    },
+    {
+      title: 'Learn English',
+      completed: false
+    },
+    {
+      title: 'Try Backbone',
+      completed: false
+    }
+  ]);
+
+  //It should log a list of al my tasks!!
+  tasks.forEach(function(model) {
+    console.log('Task title: ' + model.get('title'));
+  });
+
 })(window, document);
