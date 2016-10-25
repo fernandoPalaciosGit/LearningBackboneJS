95588fb6a9b8dfb7658a336d2ae483c416079cf0
jdavidhermoso <juandavidhermoso@gmail.com>
2016-10-25 15:51:13 +0200

    differences between ways to bind events in Backbone

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,17 +1,56 @@
 (function (w, d) {
 
-  //On() and Off() added callbacks directly to an object.
-  //listenTo() tells an object to listen for events on another object. It allows the listener to track the events for which it was listening.
-
-
-  var obj1 = _.extend({}, Backbone.Events);
-  var obj2 = _.extend({}, Backbone.Events);
-  var obj3 = _.extend({}, Backbone.Events);
-
-  obj1.listenTo(obj2, 'customEvent1', function() {
-    console.log('customEvent1 in obj2');
+  var Model = Backbone.Model.extend({
+    defaults: {
+      id: 0,
+      title: ''
+    }
   });
 
-  obj2.trigger('customEvent1');
 
+
+  var View = Backbone.View.extend({
+    //We set the div with id todo as the element of the view.
+    el: '#todo',
+
+    //This is how We bind to DOM events, using the events property
+    events: {
+      'click [type="checkbox"]': 'clicked'
+    },
+    initialize: function () {
+      //This is how We bind to DOM events using jQuery
+      this.$el.click(this.jqueryClicked);
+
+      //This how We bind to Backbone's API event
+      this.on('apiEvent', this.callback);
+
+      this.trigger('apiEvent');
+    },
+
+
+    clicked: function(event) {
+      console.log(this);
+      // 'this' keyword is the view, because this function Was called from Event bound using the view's events property
+      console.log("events handler for " + this.el.outerHTML);
+      this.trigger('apiEvent', event.type);
+    },
+
+
+    jqueryClicked: function(event) {
+      // 'this' keyword is the DOM element that is being handling.
+      console.log("jQuery handler for " + this.outerHTML);
+    },
+
+    callback: function(eventType) {
+      console.log(this);
+      console.log("event type was " + eventType);
+    }
+
+  });
+
+  var view = new View();
+  var model = new Model({
+    id:1,
+    title: 'Test'
+  });
 })(window, document);
