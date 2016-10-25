5b52850a401178fd4420872dbf70356c066ea384
jdavidhermoso <juandavidhermoso@gmail.com>
2016-10-25 12:29:25 +0200

    Playig with chains

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -16,7 +16,7 @@
     {
       id: 1,
       title: 'Try Backbone',
-      completed: false
+      completed: true
     },
     {
       id: 2,
@@ -30,54 +30,43 @@
     }
   ]);
 
+  //Chainable API.
 
-  //foreach is for iterate over collections.
+  //chain() mehtod allows to chain calls to the Underscore methods on the collections.
+  //chain() method returns an object with all Underscore functions as a method.
+  //chain ends calling to value(), and it returns the final array of values with the Underscore functions 'applied'.
 
-  //It should log a list of al my tasks!!
-  tasks.forEach(function(model) {
-    console.log('Task title: ' + model.get('title'));
-  });
+  //This statement returns a wrapper around collections model
+  var filteredTasks = tasks.chain();
+  console.log(filteredTasks);
 
-  //sortBy(): Sort a collection on a attribute.
-
-
-  var sortedAlphabetically = tasks.sortBy(function(todo) {
-    return todo.get("title").toLowerCase();
-  });
-
-  console.log("------ Collection should be sorted by alphabet now. ----");
-
-  sortedAlphabetically.forEach(function(model) {
-    console.log(model.get('title'));
-  });
-
-  //map: Iterates over a collection, and maps each value through a transformation function.
-  var count = 1;
-  console.log(tasks.map(function(model){
-    return count++ + ". " + model.get('title');
+  //Like this, I'm filtering to get only the task(s) whose completed is true, but I'm still getting the wrapper object (nor an array of models), cause I'm not appliying the value() function.
+  console.log(filteredTasks.filter(function(task) {
+    return task.get('completed') == true;
   }));
 
+  console.log('Same code than below. Unique difference is that I close the chain using value(), so I get an array of models whose completed property is true.');
+  console.log(filteredTasks.filter(function(task) {
+    return task.get('completed') == true;
+  }).value());
 
-  //min / max : Gets the item with min or max value for a property of the model.
-  var lastItem = tasks.max(function(model) {
-    return model.id
-  }).id;
-  var firstItem = tasks.min(function(model) {
-    return model.id
-  }).id;
-
-  console.log('Max: ' + lastItem);
-  console.log('Min: ' + firstItem);
-
-
-  //Pluck: Returns an array with a specific property in all the arrays
-  console.log(tasks.pluck('title'));
+  //This should return an array of titles of the tasks whose completed property is true!!
+  console.log(filteredTasks
+    .filter(function(task) {
+      return task.get('completed') == true;
+    })
+    .map(function(task) {
+      return task.get('title');
+    }).value());
 
 
-  //Filter(): Must to take another look at how it works!!
-  debugger;
-  console.log(tasks.filterById());
-  
+
+  // // start chain, returns wrapper around collection's models
+   //  .filter(function(item) { return item.get('age') > 10; }) // returns wrapped array excluding Tim
+   //  .map(function(item) { return item.get('name'); }) // returns wrapped array containing remaining names
+   //  .value(); // terminates the chain and returns the resulting array
+
+  //console.log(filteredNames); // logs: ['Ida', 'Rob']
 
 
 
