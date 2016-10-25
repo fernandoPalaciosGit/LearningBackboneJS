7b7dd847ad4e94cc85a9da8ea85f7fa230ff9468
jdavidhermoso <juandavidhermoso@gmail.com>
2016-10-25 15:13:25 +0200

    We can fire multiple events

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -3,25 +3,24 @@
 
   //We can use a "all" event, for listening any event that occurs in on the object.
   function MyHandler(evt) {
-    console.log('Event: ' + evt);
+    //This handler will be executed three times, because 3 events were fired!
+    console.log('Event: ');
   }
   _.extend(customObject, Backbone.Events);
 
-  customObject.on('all', MyHandler);
+  //Event listeners
+  customObject.on("click", MyHandler);
+  customObject.on("tap", MyHandler);
+  customObject.on("over", MyHandler);
 
 
-  $('#todoItems').on('click', function() {
-      //Trigger custom events on click to a DOM Element
-    customObject.trigger('customEvent:click', 'click!!');
+  $('#todoItems').on('click', function(evt) {
+    // We can trigger multiple events
+    customObject.trigger("click tap over", evt);
   });
 
-  $('#todoItems2').on('mouseover', function() {
-    //Trigger custom events on click to a DOM Element
-    customObject.trigger('customEvent:over', 'Hover!!');
-  });
 
-  //This time, We'll receive this event, cause We are listening ALL events over customObject
-  customObject.trigger('customEvent', 'Message!!');
+
 
 
 })(window, document);
