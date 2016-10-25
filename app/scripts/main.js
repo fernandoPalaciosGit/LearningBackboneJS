8990a1c37a2e09779047cdd9d401fba4ec0e48b5
unknown <jhermoso@VDI-EDREX64_5.brujula.es>
2016-10-25 09:42:02 +0200

    When catching a collection reset event, I can still access to the previous models

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,31 +1,20 @@
 (function (w, d) {
   //I can re-set an entire collection at once by passing a models array!!
-  var MyCollection = new Backbone.Collection();
-  MyCollection.add([
-    {
-      id: 1,
-      title: 'Learn BackboneJS',
-      completed: false
-    },
-    {
-      id: 2,
-      title: 'Eat Pizza',
-      completed: false
-    },
-    {
-      id: 3,
-      title: 'Take coffee',
-      completed: false
-    }
-  ]);
 
-  MyCollection.on("reset", function() {
-    console.log("The event fired was reset.");
-    debugger;
+  var task = new Backbone.Model({
+    title: 'My first task!'
   });
 
-  MyCollection.reset();
-  console.log(MyCollection.models);
+  var tasks = new Backbone.Collection([task])
+    .on('reset', function(todos, options) {
+      console.log(options.previousModels);
+      console.log([task]);
+      console.log(options.previousModels[0] === task); // true
+      debugger;
+    });
+
+  console.log('I am gonna reset the task model');
+  tasks.reset([]);
 
 
 })(window, document);
