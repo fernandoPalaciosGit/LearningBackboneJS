06032a273d0fd583817756c4a0e1cc5fd594576d
jdavidhermoso <juandavidhermoso@gmail.com>
2016-10-25 14:59:52 +0200

    We can use namespacing in events for readability

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,14 +1,29 @@
 (function (w, d) {
-  var customObj = {};
-// Mixin
-  _.extend(customObj, Backbone.Events);
+  var customObject = {};
 
-  //You can add a custom event
-  customObj.on('myCustomEvent', function(msg){
-    console.log('We triggered ' + msg);
+  //If We have a lof of custom events Within our application, We can add namespaced custom events.
+  function MyHandler(msg) {
+    console.log('Handler: ' + msg);
+  }
+
+  _.extend(customObject, Backbone.Events);
+
+  customObject.on('customEvent:click', MyHandler);
+  customObject.on('customEvent:over', MyHandler);
+
+
+  $('#todoItems').on('click', function() {
+      //Trigger custom events on click to a DOM Element
+    customObject.trigger('customEvent:click', 'click!!');
   });
 
-  //You can trigger the custom event
-  customObj.trigger('myCustomEvent', 'Message');
+  $('#todoItems2').on('mouseover', function() {
+    //Trigger custom events on click to a DOM Element
+    customObject.trigger('customEvent:over', 'Hover!!');
+  });
+
+  //This trigger does not trigger anything, since there isn't any listener listening for it.
+  customObject.trigger('customEvent', 'Message!!');
+
 
 })(window, document);
